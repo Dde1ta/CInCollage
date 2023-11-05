2f075d896ac8b6bfe7db6cbe84a98cc3abf6b3c6
Dde1ta <divyanshmanhasdm2005@gmail.com>
2023-11-05 22:54:59 +0530

    new file:   .vscode/launch.json 	new file:   .vscode/tasks.json 	new file:   C/Unit - 3/Exp/Exp_8_2.c 	new file:   C/Unit - 3/Exp/Exp_8_2.exe 	new file:   C/Unit - 3/Exp/exes/Exp_8_2.exe

---
+++ C/Unit - 3/Exp/Exp_8_2.c
@@ -0,0 +1,23 @@
+/*
+WAP to find max of two numbers by use the cosept of returning and passing a pointer in a function
+*/
+
+#include<stdio.h>
+
+int* max(int*,int*);
+
+int main(){
+    int a,b;
+    printf("enter the numbers");
+    scanf("%d %d",&a,&b);
+
+    printf("The max to two numbers is %d",*(max(&a,&b)));
+    return 0;
+}
+
+int* max(int *a,int *b){
+    if(*a >=*b)
+        return a;
+    else    
+        return b;
+}
