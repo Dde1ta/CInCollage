37e9a7d045640cec09f30e407e693e43ba1e998a
Dde1ta <divyanshmanhasdm2005@gmail.com>
2023-11-15 23:22:33 +0530

    modified:   C/Unit - 3/Exp/Exp_7.c 	modified:   C/Unit - 3/Exp/exes/Exp_7.exe

--- C/Unit - 3/Exp/Exp_7.c
+++ C/Unit - 3/Exp/Exp_7.c
@@ -18,6 +18,13 @@ int main(){
     printf("Enter the second string:");
     gets(str_2);
 
-    printf("The lenght of \n\tFirst string %d\n\tSecond String %d",strlen(str_1),strlen(str_2));
+    printf("The lenght of First string %d\nSecond String %d\n",strlen(str_1),strlen(str_2));
+    printf("The result of the comparision B/W them is %d\n",strcmp(str_1,str_2));
+    strcat(str_1,str_2);
+    printf("The string concatinated with each other %s\n",str_1);
+    strcpy(str_1,str_2);
+    printf("The string copied on each other %s\n",str_1);
+    strrev(str_2);
+    printf("the second string reversed %s",str_2);
     return 0;
 }
