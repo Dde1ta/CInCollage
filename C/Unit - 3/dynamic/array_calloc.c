a8ef95fac177678320c2f3b923a4fc7fb24f24da
Dde1ta <divyanshmanhasdm2005@gmail.com>
2023-11-18 12:02:48 +0530

    modified:   C/0Notes.txt 	new file:   C/Unit - 3/dynamic/array_calloc.c 	renamed:    C/Unit - 3/dynamic/array.c -> C/Unit - 3/dynamic/array_malloc.c     new file:   C/Unit - 3/dynamic/exes/array_calloc.exe

---
+++ C/Unit - 3/dynamic/array_calloc.c
@@ -0,0 +1,24 @@
+/*
+Create and tervals array using dynamic memory allocation
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+    int *p,size,i;
+    printf("Enter the size of array:");
+    scanf("%d",&size);
+
+    p = (int*)calloc(size,sizeof(int));
+    printf("enter the values of the array:");
+    for(i = 0;i<size;i++){
+        scanf("%d",p+i);
+    }
+    printf("The array is->\n");
+    for(i = 0;i<size;i++){
+        printf("%d,",*(p+i));
+    }
+    free(p);
+    return 0;
+}
