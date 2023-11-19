11711ddeae55588bacfea60817aa892e505d605b
Dde1ta <divyanshmanhasdm2005@gmail.com>
2023-11-19 17:41:32 +0530

    modified:   C/Unit - 3/Exp/Exp_10_1.c 	modified:   C/Unit - 3/Exp/Exp_10_2.c 	new file:   C/Unit - 3/Exp/exes/Exp_10_1.exe 	new file:   C/Unit - 3/Exp/exes/Exp_10_2.exe

--- C/Unit - 3/Exp/Exp_10_1.c
+++ C/Unit - 3/Exp/Exp_10_1.c
@@ -1,3 +1,23 @@
 /*
 Wap to store a string in the block of memory space created by malloc and moldfy it to store a large string
 */
+
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+    int size,i;
+    printf("Enter the size:");
+    scanf("%d",&size);
+
+    char *p = (char*)malloc((size*sizeof(char)) + 1);
+
+    printf("Enter the string");
+    for(i = 0;i<size;i++){
+        scanf("%c",p+i);
+    }
+    i = 0;
+    for(i = 0;i<size;i++){
+        printf("%c",*(p+i));
+    }
+    return 0;
+}
--- C/Unit - 3/Exp/Exp_10_2.c
+++ C/Unit - 3/Exp/Exp_10_2.c
@@ -1,3 +1,28 @@
 /*
 Wap create one array and shearh using leren sherach
 */
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+    int *p,size,to_find,i;
+    printf("Enter the size of array:");
+    scanf("%d",&size);
+
+    p = (int*)calloc(size,sizeof(int));
+    printf("enter the values of the array:");
+    for(i = 0;i<size;i++){
+        scanf("%d",p+i);
+    }
+    printf("Enter the element two find");
+    scanf("%d",&to_find);
+
+    for(i = 0;i<size;i++){
+        if(*(p+i) == to_find){
+            printf("Found at this index %d",i);
+            return 0;
+        }
+    }
+    printf("The element is not present");
+    return 0;
+}
